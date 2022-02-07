8728d0828b4a28b1ac0c572ead52d14a01aeeaa0
alonshuld <alonshuld@gmail.com>
2022-02-07 22:32:05 +0200

    1. need to clean the file before i edit it, 2. get_contant crashes at the read func

--- myfs.cpp
+++ myfs.cpp
@@ -33,28 +33,32 @@ void MyFs::create_file(std::string path_str, bool directory) {
 	if(directory)
 		throw std::runtime_error("not implemented");
 	if(path_str.length() > FILE_NAME_SIZE)
-		throw std::runtime_error("File name bigger than 10 chars");
+		throw std::runtime_error("File name bigger than 9 chars");
 	if(this->_sizeOfDir + TOTAL_FILE_SIZE > 1024 * 1024)
 		throw std::runtime_error("Disk is full");
+	for(int i = 0; i < int(this->_dir.size()); i++)
+		if(this->_dir[i].name == path_str)
+			throw std::runtime_error("File name is already taken");
 	dir_list_entry new_file;
 	new_file.name = path_str;
 	new_file.is_dir = directory;
-	new_file.file_size = 0;
-	this->blkdevsim->write(this->_sizeOfDir + 1, FILE_NAME_SIZE, path_str.c_str());
+	new_file.file_size = FILE_NAME_SIZE;
+	this->blkdevsim->write(this->_sizeOfDir + 1, new_file.name.size(), new_file.name.c_str());
 	this->_sizeOfDir += TOTAL_FILE_SIZE;
 	this->_dir.push_back(new_file); //TODO: FIX THIS PROBLEM //fixed?
 }
 
 std::string MyFs::get_content(std::string path_str) {
 	if(path_str.size() > FILE_NAME_SIZE)
-		throw std::runtime_error("File name is bigger than 10");
+		throw std::runtime_error("File name is bigger than 9");
 	char* ans = NULL;
 	int addr = sizeof(myfs_header) + FILE_NAME_SIZE + 1;
-	for(int i = 0; i < this->_dir.size(); i++)
+	for(int i = 0; i < int(this->_dir.size()); i++)
 	{
 		if(this->_dir[i].name == path_str)
 		{
-			this->blkdevsim->read(addr, this->_dir[i].file_size - FILE_NAME_SIZE, ans);
+			this->blkdevsim->read(addr, this->_dir[i].file_size - FILE_NAME_SIZE + 1, ans);
+			std::cout << "test" << std::endl;
 			break;
 		}
 		else
@@ -63,7 +67,10 @@ std::string MyFs::get_content(std::string path_str) {
 	if(ans == NULL)
 		throw std::runtime_error("File not found");
 	else
-		return std::string(ans);
+	{
+		std::string ansString(ans);
+		return ansString;
+	}
 }
 
 void MyFs::set_content(std::string path_str, std::string content) {
@@ -72,21 +79,22 @@ void MyFs::set_content(std::string path_str, std::string content) {
 	if(content.size() > TOTAL_FILE_SIZE - FILE_NAME_SIZE)
 		throw std::runtime_error("The contant of the file must be 1014 or less");
 	int addr = sizeof(myfs_header) + FILE_NAME_SIZE + 1;
-	for(int i = 0; i < this->_dir.size(); i++)
+	for(int i = 0; i < int(this->_dir.size()); i++)
 	{
 		if(this->_dir[i].name == path_str)
 		{
-			this->blkdevsim->write(addr, TOTAL_FILE_SIZE - FILE_NAME_SIZE, content.c_str());
+			this->blkdevsim->write(addr, TOTAL_FILE_SIZE - FILE_NAME_SIZE, NULL);
+			this->blkdevsim->write(addr, content.size(), content.c_str());
 			this->_dir[i].file_size = FILE_NAME_SIZE + content.size();
+			return;
 		}
 		else
 			addr += TOTAL_FILE_SIZE;
 	}
+	throw std::runtime_error("File not found");
 }
 
 MyFs::dir_list MyFs::list_dir(std::string path_str) {
-	if(path_str.size())
-		throw std::runtime_error("Not implemented");
 	return this->_dir;
 }
 
